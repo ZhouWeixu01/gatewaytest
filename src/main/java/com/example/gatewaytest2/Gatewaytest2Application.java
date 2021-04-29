90893c8e9bf5acadd19391fefbf4936a5a8dd6a6
zhouweixu <zhouweixu@tianan-life.com>
2021-04-29 16:30:42 +0800

    spring gateway 练习

---
+++ src/main/java/com/example/gatewaytest2/Gatewaytest2Application.java
@@ -0,0 +1,13 @@
+package com.example.gatewaytest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gatewaytest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gatewaytest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gatewaytest2/Gatewaytest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewaytest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gatewaytest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
